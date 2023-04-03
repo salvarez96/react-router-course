ae0e2479329c128c44fdecc10aa94738051c27cf
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2023-04-02 20:48:36 -0500

    Class 6: create the first route components of the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { HashRouter, Routes, Route } from "react-router-dom"
+import Menu from "./components/menu"
+import BlogPage from "./pages/blog-page"
+import HomePage from "./pages/home-page"
+import ProfilePage from "./pages/profile-page"
 
 function App() {
 
   return (
     <>
-    
+      <HashRouter>
+        <Menu />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="*" element={<p>Not found</p>} />
+        </Routes>
+      </HashRouter>
     </>
   )
 }
