f9994a353d7198207b9fd47c7e8164a9a9485be5
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2023-04-02 23:04:13 -0500

    Class 7: create nav links using NavLink RRD special component

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,7 +1,39 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const navLinks = [
+  {
+    to: '/',
+    text: 'Home'
+  },
+  {
+    to: '/blog',
+    text: 'Blog'
+  },
+  {
+    to: '/profile',
+    text: 'Profile'
+  }
+]
 
 export default function Menu() {
   return (
-    <nav>Menu</nav>
+    <nav>
+      <ul>
+        {navLinks.map(({ to, text }, i) => (
+          <li key={i}>
+            <NavLink 
+              style={({ isActive }) => (
+                { 
+                  color: isActive ? 'red' : 'black'
+                }
+              )} 
+              to={to}>
+              {text}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
   )
 }
